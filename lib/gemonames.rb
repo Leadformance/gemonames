3ebd686e040dafdfa0ed57f6ecd02265dcc5d11f
Miha Filej <miha@filej.net>
2015-10-09 10:14:01 +0200

    Simple wrapper around searchJSON

--- lib/gemonames.rb
+++ lib/gemonames.rb
@@ -1,4 +1,68 @@
 require "gemonames/version"
+require "values"
+require "faraday"
+require "faraday_middleware"
 
 module Gemonames
+  module_function
+  BASE_API_URL = "http://api.geonames.org"
+
+  def client(username:, connection: nil)
+    connection ||= build_connection(username: username)
+    ApiClient.new(connection)
+  end
+
+  def build_connection(username:)
+    Faraday.new(url: BASE_API_URL) do |faraday|
+      faraday.response :json
+      faraday.params[:username] = username
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  class ApiClient
+    attr_reader :connection
+
+    def initialize(connection)
+      @connection = connection
+    end
+
+    def search(query, country_code:)
+      results = connection.get do |request|
+        request.url "/searchJSON".freeze
+        request.params[:q] = query
+        request.params[:country] = country_code
+        request.params[:maxRows] = 1
+        request.params[:style] = "short".freeze
+      end
+
+      result = results.body.fetch("geonames").first
+
+      if result
+        SearchResult.with(
+          geoname_id: result.fetch("geonameId"),
+          name: result.fetch("name"),
+          country_code: result.fetch("countryCode"),
+        )
+      else
+        NoSearchResult.new
+      end
+    end
+  end
+
+  SearchResult = Value.new(:geoname_id, :name, :country_code) do
+    def result?
+      true
+    end
+  end
+
+  class NoSearchResult
+    def geoname_id() end
+    def name() end
+    def country_code() end
+
+    def result?
+      false
+    end
+  end
 end
--- spec/gemonames_spec.rb
+++ spec/gemonames_spec.rb
@@ -1,7 +1,24 @@
 require "spec_helper"
 
 describe Gemonames do
-  it "has a version number" do
-    expect(Gemonames::VERSION).not_to be nil
+  let(:client) { Gemonames.client(username: "demo") }
+
+  it "performs a search based on city and country code" do
+    result = client.search("Celje", country_code: "si")
+
+    aggregate_failures do
+      expect(result.result?).to be_truthy
+      expect(result).to have_attributes(
+        geoname_id: 3202781,
+        name: "Celje",
+        country_code: "SI",
+      )
+    end
+  end
+
+  it "returns an empty result" do
+    result = client.search("UnknownPlace", country_code: "si")
+
+    expect(result.result?).to be_falsey
   end
 end

d6699c4066458705c7b906f225227b34874aead3
Miha Filej <miha@filej.net>
2015-12-03 11:26:23 +0100

    Default to empty results when geonames key missing

--- spec/gemonames_spec.rb
+++ spec/gemonames_spec.rb
@@ -31,6 +31,20 @@ describe Gemonames do
 
       expect(results).to be_empty
     end
+
+    it "returns empty result when http response not successful" do
+      connection = double "connection",
+        get: instance_double(
+          "Faraday::Response",
+          env: { status: 400 },
+          body: {}
+      )
+      client = Gemonames.client(username: "demo", connection: connection)
+
+      results = client.search("query", country_code: "si")
+
+      expect(results).to be_empty
+    end
   end
 
   describe "#find" do
@@ -58,6 +72,20 @@ describe Gemonames do
       result = VCR.use_cassette "find-no-result" do
         client.find("UnknownPlace", country_code: "si")
       end
+
+      expect(result.result?).to be_falsey
+    end
+
+    it "returns empty result when http response not successful" do
+      connection = double "connection",
+        get: instance_double(
+          "Faraday::Response",
+          env: { status: 400 },
+          body: {}
+      )
+      client = Gemonames.client(username: "demo", connection: connection)
+
+      result = client.find("query", country_code: "si")
 
       expect(result.result?).to be_falsey
     end
